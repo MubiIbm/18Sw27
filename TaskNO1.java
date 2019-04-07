701be382e89f4fc9a2d01f5c90d80674e8c32730
Mubashir Ibrahim <49161541+Mubashirsw27@users.noreply.github.com>
2019-04-07 12:18:00 +0500

    Add files via upload
    Mam These are the You Have given to me.
    From 18SW27

---
+++ TaskNO1.java
@@ -0,0 +1,29 @@
+
+class TaskNO1{
+	public static void main(String args[])
+	{
+		String name="Mubashir";
+		int rollno=27;
+		float percentage=75;
+		char grade = 65;
+		boolean pass=true;
+		boolean fail=false;
+		short semesno=2;
+		long days=187;
+		double age=19.5;
+		System.out.println("Name = "+name);
+		System.out.println("Roll Number = 18SW"+rollno);
+		System.out.println("My Grade = "+grade);
+		if(percentage>50)
+		{
+			System.out.println("I am Passed "+pass);
+		}
+		else
+		{
+			System.out.println("I am Failed "+fail);
+		}
+		System.out.println("I am studying in "+semesno+" Semester");
+		System.out.println("I have Spended "+days+" days in University");
+		System.out.println("My Age is "+age+" Years");
+	}
+}
---
+++ TaskNO2.java
@@ -0,0 +1,12 @@
+class TaskNO2
+{
+	public static void main(String args[])
+	{
+		double a=50.25;
+		long b=(long)a;
+		double c=(double)a-b;
+		System.out.println("Original Number is "+a);
+		System.out.println("Fractional Part = "+c);
+		System.out.println("Integral Part = "+b);
+	}
+}
