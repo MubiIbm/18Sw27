9e3ca92b5526d1d9a72a2d00bd5aff71b07e11a2
Mubashir Ibrahim <49161541+Mubashirsw27@users.noreply.github.com>
2019-04-21 16:06:56 +0500

    Add files via upload

---
+++ Binary To Decimal Converter(By Logic)/Bina.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+class Bina{    
+public static long Decimal(long num){   
+     long b = 0;  
+     long deci = 0; 
+     boolean i=true;
+    while(i==true){  
+      if(num == 0){  
+        break;  
+      } 
+      else {  
+          long temp = num%10;  
+          deci += temp*Math.pow(2, b);  
+          num = num/10;  
+          b++;  
+       }  
+    }  
+    return deci;  
+}  
+public static void main(String args[]){ 
+Scanner sc = new Scanner(System.in); 
+long a;
+System.out.print("Enter Number in Binary = ");
+a=sc.nextLong();
+System.out.println("Decimal Number of Given Number is = "+Decimal(a));  
+}
+}
